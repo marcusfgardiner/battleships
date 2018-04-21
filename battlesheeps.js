7d8003df6766a9f044ac2fa17948d3254a6e1b37
marcusfgardiner <marcusfgardiner@gmail.com>
2018-04-21 12:53:19 +0100

    feat: creates battleships gris

--- battlesheeps.js
+++ battlesheeps.js
@@ -1,21 +1,23 @@
-var BattleshipGrid = (n, array) => {
-  this.outputString = ''
-  this.outputString = buildOutputString(n, array, outputString);
+let battleshipGrid = (n, array) => {
+  let outputString = ''
+  outputString = buildOutputString(n, array, outputString);
+  return outputString
 }
 
-BattleshipGrid.prototype.buildOutputString = (n, array, outputString) => {
+let buildOutputString = (n, array, outputString) => {
   array.map(function(element, i) {
-    outputString += this.dotsStars(element);
-    if (this.isNewGridLine(i, n, array))
-      { this.outputString += '\n' }
+    outputString += dotsStars(element);
+    if (isNewGridLine(i, n, array))
+      { outputString += '\n' }
   });
+  return outputString
 }
 
-BattleshipGrid.prototype.isNewGridLine = (i, n, array) => {
+let isNewGridLine = (i, n, array) => {
   return ((i+1) % n === 0 && ((i+1) < array.length))
 };
 
-BattleshipGrid.prototype.dotsStars = element => {
+let dotsStars = element => {
   if ( element === 0 ) {
     return '.'
   }
@@ -33,4 +35,4 @@ let test = (n, array) => {
   else { console.log('Test failed') }
 };
 
-test(3, [0, 1, 0, 1,0,0,0,1,0]);
+test(2, [0, 1, 0, 1]);
